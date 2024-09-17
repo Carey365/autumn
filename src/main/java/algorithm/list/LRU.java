1410f5da5a3a35a8a0188f3d5a60f0a385b5e87f
Carey <clh@bupt.edu.cn>
2024-09-17 23:08:06 +0800

    codes for amazon example

---
+++ src/main/java/algorithm/list/LRU.java
@@ -0,0 +1,90 @@
+package algorithm.list;
+import java.util.*;
+
+public class LRU {
+    ListNode dummyHead;
+    ListNode dummyTail;
+    Map<Integer,ListNode> map;
+    int size;
+    int capacity;
+
+    private class ListNode {
+        int key;
+        int value;
+        ListNode pre;
+        ListNode next;
+
+        ListNode() {
+        }
+        ListNode(int key,int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    LRU(int capacity){
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        this.dummyHead = new ListNode();
+        this.dummyTail = new ListNode();
+        dummyHead.next = dummyTail;
+        dummyTail.pre = dummyHead;
+    }
+
+    public int get(int key){
+        if(!map.containsKey(key)){
+            return -1;
+        }
+        moveToHead(key);
+        return map.get(key).value;
+    }
+
+    public void add(int key,int value){
+        if(map.containsKey(key)){
+            removeNode(key);
+        }
+        else if(size>=capacity){
+            removeTail();
+        }
+        addToHead(key,value);
+    }
+
+
+    private void removeTail(){
+        ListNode tail = dummyTail.pre;
+        ListNode newTail = tail.pre;
+        newTail.next = dummyTail;
+        dummyTail.pre = newTail;
+        map.remove(tail.key);
+        size--;
+    }
+
+    private void removeNode(int key){
+        ListNode node = map.get(key);
+        ListNode pre = node.pre;
+        ListNode next = node.next;
+        pre.next = next;
+        next.pre = pre;
+        map.remove(key);
+        size--;
+    }
+
+    private void addToHead(int key,int value){
+        ListNode head = dummyHead.next;
+        ListNode newHead = new ListNode(key,value);
+        map.put(key,newHead);
+        dummyHead.next = newHead;
+        newHead.pre = dummyHead;
+        newHead.next = head;
+        head.pre = newHead;
+        size++;
+    }
+
+    private void moveToHead(int key){
+        ListNode node = map.get(key);
+        removeNode(key);
+        addToHead(key,node.value);
+    }
+
+
+}
--- src/main/java/algorithm/list/ListNodeTest.java
+++ src/main/java/algorithm/list/ListNodeTest.java
@@ -3,16 +3,27 @@ package algorithm.list;
 public class ListNodeTest {
 
     public static void main(String[] args) {
-        //初始化链表
-        int[] nums = new int[]{1,2,3,4,5,6};
-        ListNode node = ListNode.buildList(nums);
-        //打印链表长度
-        System.out.println(ListNode.getLength(node));
-        //打印链表
-        ListNode.printList(node);
-        //翻转链表
-        ListNode reverse = ListNode.reverse(node);
-        //打印翻转后的链表
-        ListNode.printList(reverse);
+//        //初始化链表
+//        int[] nums = new int[]{1,2,3,4,5,6};
+//        ListNode node = ListNode.buildList(nums);
+//        //打印链表长度
+//        System.out.println(ListNode.getLength(node));
+//        //打印链表
+//        ListNode.printList(node);
+//        //翻转链表
+//        ListNode reverse = ListNode.reverse(node);
+//        //打印翻转后的链表
+//        ListNode.printList(reverse);
+        LRU lru = new LRU(3);
+        lru.add(1,2);
+        lru.add(2,3);
+        lru.add(3,4);
+        lru.add(4,5);
+        System.out.println(lru.get(1));
+        System.out.println(lru.get(2));
+        System.out.println(lru.get(3));
+        System.out.println(lru.get(4));
+        lru.add(1,2);
+        System.out.println(lru.get(2));
     }
 }
